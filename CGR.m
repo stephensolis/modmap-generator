d4bc400d0d57db0c5d120dbe6a5c3e2de9958cc2
Stephen <stephen@nowhere>
2016-01-28 15:07:40 -0500

    Greatly improved dense CGR performance

--- CGR.m
+++ CGR.m
@@ -11,6 +11,41 @@ SparseFCGR::usage =
 
 Begin["`Private`"]
 
+FCGR[seq_String, cgrOrder_List, k_Integer?Positive]:=
+	Module[{chars},
+		chars = ToCharacterCode[StringReplace[seq, Except[cgrOrder]->""]];
+		
+		Return[FCGRHelper[chars, Flatten@ToCharacterCode[cgrOrder], k]];
+	];
+
+FCGRHelper = Compile[{{chars, _Integer, 1}, {cgrOrder, _Integer, 1}, {k, _Integer}},
+	Module[{resimg, x, y, char, i},
+		resimg = ConstantArray[0, {2^k, 2^k}];
+		
+		x = 2^(k-1);
+		y = 2^(k-1);
+		Do[
+			char = chars[[i]];
+			
+			x = Quotient[x, 2];
+			If[char == cgrOrder[[3]] || char == cgrOrder[[4]], 
+				x += 2^(k-1);
+			];
+			
+			y = Quotient[y, 2];
+			If[char == cgrOrder[[1]] || char == cgrOrder[[4]], 
+				y += 2^(k-1);
+			];
+			
+			If[i >= k,
+				resimg[[y+1, x+1]]++;
+			];
+		, {i, Length@chars}];
+		
+		resimg
+	]
+, RuntimeOptions -> "Speed"];
+
 SparseFCGR[seq_String, {a1_, a2_, a3_, a4_}, k_Integer?Positive]:=
 	Module[{shifts, pts, arrayrules, resimg},
 		shifts = Characters[seq] /. {a1->{0, 0}, a2->{0, 2^k}, a3->{2^k, 2^k}, a4->{2^k, 0}, _?StringQ->Sequence[]};
@@ -23,8 +58,6 @@ SparseFCGR[seq_String, {a1_, a2_, a3_, a4_}, k_Integer?Positive]:=
 		
 		Return[resimg];
 	];
-
-FCGR = Normal @* SparseFCGR;
 
 
 End[]
